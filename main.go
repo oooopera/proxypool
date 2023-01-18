8349c9ff3c6dd7317dd94a4f29f1bd7863b4b2a1
oooopera <sweetbox-sx@qq.com>
2023-01-18 15:52:23 +0800

    1

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	_ "net/http/pprof"
+	"os"
+
+	"github.com/oooopera/proxypool/config"
+	"github.com/oooopera/proxypool/pkg/geoIp"
+
+	"github.com/oooopera/proxypool/api"
+	"github.com/oooopera/proxypool/internal/app"
+	"github.com/oooopera/proxypool/internal/cron"
+	"github.com/oooopera/proxypool/internal/database"
+	"github.com/oooopera/proxypool/log"
+)
+
+var debugMode = false
+
+func main() {
+	var configFilePath = ""
+
+	//go func() {
+	//	http.ListenAndServe("0.0.0.0:6060", nil)
+	//}()
+
+	flag.StringVar(&configFilePath, "c", "", "path to config file: config.yaml")
+	flag.BoolVar(&debugMode, "d", false, "debug output")
+	flag.Parse()
+
+	log.SetLevel(log.INFO)
+	if debugMode {
+		log.SetLevel(log.DEBUG)
+		log.Debugln("=======Debug Mode=======")
+	}
+	if configFilePath == "" {
+		configFilePath = os.Getenv("CONFIG_FILE")
+	}
+	if configFilePath == "" {
+		configFilePath = "config.yaml"
+	}
+
+	config.SetFilePath(configFilePath)
+
+	err := app.InitConfigAndGetters()
+	if err != nil {
+		log.Errorln("Configuration init error: %s", err.Error())
+		panic(err)
+	}
+
+	exe, _ := os.Executable()
+	log.Infoln("Running image path: %s", exe)
+
+	database.InitTables()
+	// init GeoIp db reader and map between emoji's and countries
+	// return: struct geoIp (dbreader, emojimap)
+	err = geoIp.InitGeoIpDB()
+	if err != nil {
+		os.Exit(1)
+	}
+	log.Infoln("Do the first crawl...")
+	go app.CrawlGo() // 抓取主程序
+	go cron.Cron()   // 定时运行
+	api.Run()        // Web Serve
+}
---
+++ pkg/geoIp/geoip.go
@@ -0,0 +1,110 @@
+package geoIp
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+
+	"github.com/oschwald/geoip2-golang"
+	"github.com/oooopera/proxypool/config"
+	bingeoip "github.com/oooopera/proxypool/internal/bindata/geoip"
+)
+
+var GeoIpDB GeoIP
+
+func InitGeoIpDB() error {
+	parentPath := config.ResourceRoot()
+	geodbPath := "assets/GeoLite2-City.mmdb"
+	flagsPath := "assets/flags.json"
+	geodb := filepath.Join(parentPath, geodbPath)
+	flags := filepath.Join(parentPath, flagsPath)
+	// 判断文件是否存在
+	_, err := os.Stat(geodb)
+	if err != nil && os.IsNotExist(err) {
+		err = bingeoip.RestoreAsset("", flagsPath)
+		if err != nil {
+			panic(err)
+		}
+		err = bingeoip.RestoreAsset("", geodbPath)
+		if err != nil {
+			log.Println("文件不存在, 请自行下载 Geoip2 City 库, 并保存在", geodb)
+			panic(err)
+		}
+		GeoIpDB = NewGeoIP(geodb, flags)
+	}
+	GeoIpDB = NewGeoIP(geodb, flags)
+	return nil
+}
+
+// GeoIP2
+type GeoIP struct {
+	db       *geoip2.Reader
+	emojiMap map[string]string
+}
+
+type CountryEmoji struct {
+	Code  string `json:"code"`
+	Emoji string `json:"emoji"`
+}
+
+// new geoip from db file
+func NewGeoIP(geodb, flags string) (geoip GeoIP) {
+	// 运行到这里时geodb只能为存在
+	db, err := geoip2.Open(geodb)
+	if err != nil {
+		log.Fatal(err)
+	}
+	geoip.db = db
+
+	_, err = os.Stat(flags)
+	if err != nil && os.IsNotExist(err) {
+		log.Println("flags 文件不存在, 请自行下载 flags.json, 并保存在 ", flags)
+		os.Exit(1)
+	} else {
+		data, err := ioutil.ReadFile(flags)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		var countryEmojiList = make([]CountryEmoji, 0)
+		err = json.Unmarshal(data, &countryEmojiList)
+		if err != nil {
+			log.Fatalln(err.Error())
+			return
+		}
+
+		emojiMap := make(map[string]string)
+		for _, i := range countryEmojiList {
+			emojiMap[i.Code] = i.Emoji
+		}
+		geoip.emojiMap = emojiMap
+	}
+	return
+}
+
+// find ip info
+func (g GeoIP) Find(ipORdomain string) (ip, country string, err error) {
+	ips, err := net.LookupIP(ipORdomain)
+	if err != nil {
+		return "", "", err
+	}
+	ip = ips[0].String()
+
+	var record *geoip2.City
+	record, err = g.db.City(ips[0])
+	if err != nil {
+		return
+	}
+	countryIsoCode := record.Country.IsoCode
+	emoji, found := g.emojiMap[countryIsoCode]
+	if found {
+		country = fmt.Sprintf("%v%v", emoji, countryIsoCode)
+	} else {
+		country = "🏁ZZ"
+	}
+	return ip, country, err
+}
